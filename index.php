a9c39a7cf0279b751022f9c71308b22c4bf81e3f
Marius L <mariusleica@gmail.com>
2022-07-12 17:17:43 +0200

    First millestone done

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+require_once __DIR__ . "/db.php";
+
+//Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
+
 
 ?>
 
@@ -9,10 +13,50 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0-beta1/css/bootstrap.min.css' integrity='sha512-o/MhoRPVLExxZjCFVBsm17Pkztkzmh7Dp8k7/3JrtNCHh0AQ489kwpfA3dPSHzKDe8YCuEhxXq3Y71eb/o6amg==' crossorigin='anonymous' />
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 
 <body>
+    <div class="bg-dark p-5">
+        <div class="container flex-grow-1">
+            <div class="pt-5 pb-2 px-1">
+                <div class="row row-cols-2 row-cols-md-5">
+                    <?php
+                    foreach ($albumsList as $album) {
+                        echo "<div class='col mb-3'>";
+                        echo  "<div class='card text-center h-100'>";
+                        echo "<img class='card-img-top img-poster' src='$album[poster]' alt='$album[title]'>";
+                        echo "<div class='card-body'>";
+                        echo "<h5 class='card-title'>$album[title]</h5>";
+                        echo "<div class='text-muted'>";
+                        echo  "<h6>$album[author]</h6>";
+                        echo "</div>";
+                        echo "<div class='text-muted'>";
+                        echo "<span  class='fs-6'>$album[year]</span>";
+                        echo "</div>";
+                        echo "</div>";
+                        echo "</div>";
+                        echo "</div>";
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
 
 </body>
 
